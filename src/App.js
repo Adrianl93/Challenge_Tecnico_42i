1c871106a3ee67c1f16d15d3de94b3fbff37c07c
Adrianl93 <laborde.adrian2012@gmail.com>
2024-01-28 20:10:22 -0300

    Eslint fix installed and run

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
+import React from 'react';
 import './App.css';
-
-import Home from "./components/Home"
-
+import Home from './components/Home';
 function App() {
   return (
     <>
+
       <Home/>
     </>
   );
